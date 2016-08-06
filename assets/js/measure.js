7db8ff59de853d989271148d4c7b78922b111ff5
Pavel Ševčík <pajaseviwow@gmail.com>
2016-08-06 17:54:48 +0200

    Measure fix

--- assets/js/measure.js
+++ assets/js/measure.js
@@ -1,29 +1,30 @@
-(function($, ga) {
   // Track newsletter form submit
-  $('#newsletterForm').on('submit', function() {
+  document.getElementById('newsletterForm').addEventListener('submit', function() {
     ga('send', {
       hitType: 'event',
       eventCategory: 'Newsletter',
       eventAction: 'subscribe',
       transport: 'beacon'
     });
-  });
+  }, false);
 
   // Track speaker click
-  $('.speaker-item').on('click', function() {
-    ga('send', {
-      hitType: 'event',
-      eventCategory: 'Speakers',
-      eventAction: 'click'
-    });
+  var speakerItems = document.querySelectorAll('.speaker-item');
+  Array.prototype.forEach.call(speakerItems, function(speakerItem) {
+    speakerItem.addEventListener('click', function(event) {
+      ga('send', {
+        hitType: 'event',
+        eventCategory: 'Speakers',
+        eventAction: 'click'
+      });
+    }, false);
   });
 
   // Track open more Speakers
-  $('.see-more').on('click', function() {
+  document.querySelector('.see-more').addEventListener('click', function() {
     ga('send', {
       hitType: 'event',
       eventCategory: 'Speakers',
       eventAction: 'open'
-    });
+    }, false);
   });
-})(jQuery, ga);

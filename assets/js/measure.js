a7bf3d37b4006e8fd83b3f5efafd6ffa234f0cea
Pavel Ševčík <pajaseviwow@gmail.com>
2016-08-10 13:55:55 +0200

    Updated measurement

--- assets/js/measure.js
+++ assets/js/measure.js
@@ -8,6 +8,32 @@
     });
   }, false);
 
+  // Track main navigation clicks
+  var navLinks = document.querySelectorAll('.nav a');
+  Array.prototype.forEach.call(navLinks, function(navLink) {
+    navLink.addEventListener('click', function(event) {
+      ga('send', {
+        hitType: 'event',
+        eventCategory: 'Navigation',
+        eventAction: 'click',
+        eventLabel: navLink.textContent
+      });
+    }, false);
+  });
+
+  // Track footer navigation clicks
+  var footerNavLinks = document.querySelectorAll('.footer-nav a');
+  Array.prototype.forEach.call(footerNavLinks, function(footerNavLink) {
+    footerNavLink.addEventListener('click', function(event) {
+      ga('send', {
+        hitType: 'event',
+        eventCategory: 'Navigation',
+        eventAction: 'click',
+        eventLabel: footerNavLink.textContent
+      });
+    }, false);
+  });
+
   // Track speaker click
   var speakerItems = document.querySelectorAll('.speaker-item');
   Array.prototype.forEach.call(speakerItems, function(speakerItem) {
@@ -15,7 +41,8 @@
       ga('send', {
         hitType: 'event',
         eventCategory: 'Speakers',
-        eventAction: 'click'
+        eventAction: 'click',
+        eventLabel: speakerItem.querySelector('.speaker-accent').textContent
       });
     }, false);
   });
